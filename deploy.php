9c8ee6c50c44ec90ef6ba0caa81152a29239da6c
ak@scandesignmedia.dk <ak@scandesignmedia.dk>
2021-10-31 19:02:46 +0100

    deployer

--- deploy.php
+++ deploy.php
@@ -1,7 +1,9 @@
 <?php
 namespace Deployer;
 
-$deployPath = '/var/www/{{application}}';
+require 'recipe/common.php';
+
+$deployPath = '/home/magento_user/var/www/{{application}}';
 $contentVersion = time();
 
 host('49.12.197.154')
@@ -13,6 +15,11 @@ host('49.12.197.154')
     ->set('deploy_path', $deployPath)
     ->set('http_user', 'magento_user')
     ->set('branch', 'master')
+    ->set('nfs_root', '/home/magento_user/var/mnt/nfs')
+    ->set('nfs_dirs', ['pub/media'])
+    ->forwardAgent()
+    ->addSshOption('StrictHostKeyChecking', 'no')
+
 ;
 // Project name
 set('application', 'magento_pwa');
@@ -69,27 +76,37 @@ task('cron:stop', function () {
     run("sudo systemctl stop cron.service");
 });
 
+desc('Copies ENV');
+task('magento:copy:env', function () {
+    run("cp -p {{deploy_path}}/env.php {{release_path}}/app/etc/env.php");
+});
+
 desc('deploy vendors');
 task('deploy:vendors', function () {
-    cd('{{deploy_path}}');
+    cd('{{release_path}}');
     run("{{bin/composer}} install --no-dev");
+});
+
+desc('Module enable');
+task('magento:module:enable', function () {
+    run("{{bin/php}} {{release_path}}/bin/magento module:enable --all");
 });
 
 desc('Production mode');
 task('magento:production:mode', function () {
-    run("{{bin/php}} {{deploy_path}}/bin/magento deploy:mode:set production -s");
+    run("{{bin/php}} {{release_path}}/bin/magento deploy:mode:set production -s");
 });
 
 desc('Compile magento di');
 task('magento:compile', function () {
-    run("{{bin/php}} {{deploy_path}}/bin/magento setup:di:compile");
-    run('cd {{deploy_path}} && {{bin/composer}} dump-autoload -o --apcu');
+    run("{{bin/php}} {{release_path}}/bin/magento setup:di:compile");
+    run('cd {{release_path}} && {{bin/composer}} dump-autoload -o --apcu');
 });
 
 desc('Deploy assets');
 task('magento:deploy:assets', function () use ($contentVersion) {
     run(
-        "{{bin/php}} {{deploy_path}}/bin/magento setup:static-content:deploy {{static_content_locales}}"
+        "{{bin/php}} {{release_path}}/bin/magento setup:static-content:deploy {{static_content_locales}}"
         . " --jobs 50"
         . " --content-version=" . $contentVersion
     );
@@ -97,26 +114,28 @@ task('magento:deploy:assets', function () use ($contentVersion) {
 
 desc('Enable maintenance mode');
 task('magento:maintenance:enable', function () {
-    run("if [ -d $(echo {{deploy_path}}) ]; then {{bin/php}} {{deploy_path}}/bin/magento maintenance:enable; fi");
+    run("if [ -d $(echo {{release_path}}) ]; then {{bin/php}} {{release_path}}/bin/magento maintenance:enable; fi");
 });
 
 desc('Upgrade magento database');
 task('magento:upgrade:db', function () {
-    run("{{bin/php}} {{deploy_path}}/bin/magento setup:upgrade --keep-generated");
+    run("{{bin/php}} {{release_path}}/bin/magento setup:upgrade --keep-generated");
 });
 
 desc('Flush Magento Cache');
 task('magento:cache:flush', function () {
-    run("{{bin/php}} {{deploy_path}}/bin/magento cache:flush");
+    run("{{bin/php}} {{release_path}}/bin/magento cache:flush");
 });
 
 desc('Disable maintenance mode');
 task('magento:maintenance:disable', function () {
-    run("if [ -d $(echo {{deploy_path}}) ]; then {{bin/php}} {{deploy_path}}/bin/magento maintenance:disable; fi");
+    run("if [ -d $(echo {{deploy_path}}) ]; then {{bin/php}} {{release_path}}/bin/magento maintenance:disable; fi");
 });
 
 desc('Magento2 deployment operations');
 task('deploy:magento', [
+    'magento:module:enable',
+    'magento:copy:env',
     'magento:production:mode',
     'magento:compile',
     'magento:deploy:assets',
@@ -128,9 +147,9 @@ task('deploy:magento', [
 
 desc('Change Chmod');
 task('magento:change:chmod', function () {
-    run("sudo chmod 775 -R {{deploy_path}}/generated");
-    run("sudo chmod 775 -R {{deploy_path}}/var");
-    run("sudo chmod 775 -R {{deploy_path}}/pub/static");
+    run("chmod 775 -R {{release_path}}/generated");
+    run("chmod 775 -R {{release_path}}/var");
+    run("chmod 775 -R {{release_path}}/pub/static");
 });
 
 desc('Start cron');
@@ -141,26 +160,23 @@ task('cron:start', function () {
 
 desc('Deploy your project');
 task('deploy', [
+    'deploy:unlock',
     'cron:stop',
     'deploy:info',
     'deploy:prepare',
     'deploy:lock',
+    'deploy:release',
     'deploy:update_code',
+    'deploy:shared',
     'deploy:writable',
     'deploy:vendors',
     'deploy:clear_paths',
     'deploy:magento',
     'magento:change:chmod',
+    'deploy:symlink',
     'deploy:unlock',
     'cron:start',
     'success'
 ]);
 
 before('deploy', 'notify:time');
-
-after('deploy:failed', 'magento:maintenance:disable');
-after('deploy:failed', 'cron:start');
-
-after('success', 'restart:services');
-after('success', 'cleanup');
-after('success', 'notify:time');
